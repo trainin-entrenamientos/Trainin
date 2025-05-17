e34fe74388a1a707ff5f6436041570e481523460
iv-au <ivancarr03@gmail.com>
2025-05-17 16:50:58 -0300

    Cambio de semana y dia, boton iniciar rutina agregado

--- src/app/vistas/detalle-plan/detalle-plan.component.ts
+++ src/app/vistas/detalle-plan/detalle-plan.component.ts
@@ -7,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrl: './detalle-plan.component.css'
 })
 export class DetallePlanComponent {
+  semanas: any[] = [];
+  semanaActual = 0;
+  diaActivo = 0;
 
+  constructor(){
+    this.mockearDatos();
+  }
+
+  mockearDatos(){
+    this.semanas = [
+      {
+        numero: 1,
+        dias: ['Día 1', 'Día 2', 'Día 3']
+      },
+      {
+        numero: 2,
+        dias: ['Día 1', 'Día 2', 'Día 3']
+      },
+      {
+        numero: 3,
+        dias: ['Día 1', 'Día 2', 'Día 3']
+      }
+    ];
+  }
+
+  cambiarSemana(offset: number){
+    const nuevaSemana = this.semanaActual + offset;
+    if (nuevaSemana >= 0 && nuevaSemana < this.semanas.length) {
+      this.semanaActual = nuevaSemana;
+      this.diaActivo = 0;
+    }
+  }
+
+  seleccionarDia(index: number){
+    this.diaActivo = index;
+  }
+
+  get semanaSeleccionada(){
+    return this.semanas[this.semanaActual];
+  }
 }
