0499a0ac6448cc30a9e51f6ce6f0017a60a99391
Camila <pereyra.camila@outlook.com>
2025-05-28 04:05:31 -0300

    Subo ajustes header, footer y plan de entrenamiento. Login y registro con css dinamico.

--- src/app/vistas/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/vistas/iniciar-sesion/iniciar-sesion.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   selector: 'app-iniciar-sesion',
   templateUrl: './iniciar-sesion.component.html',
   styleUrl: './iniciar-sesion.component.css',
-  standalone:false
+  standalone: false
 })
 export class IniciarSesionComponent {
   loginForm: FormGroup;
@@ -18,34 +18,34 @@ export class IniciarSesionComponent {
     private router: Router
   ) {
     this.loginForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    contrasenia: ['', Validators.required]
-});
+      email: ['', [Validators.required, Validators.email]],
+      contrasenia: ['', Validators.required]
+    });
 
   }
 
   iniciarSesion() {
-  const credenciales = this.loginForm.value;
+    const credenciales = this.loginForm.value;
 
-  this.authService.login(credenciales).subscribe({
-    next: (data) => {
-      console.log('Login exitoso:', data);
+    this.authService.login(credenciales).subscribe({
+      next: (data) => {
+        console.log('Login exitoso:', data);
 
-      if (data.exito && !data.requiereActivacion) {
-        localStorage.setItem('token', data.token);
-        localStorage.setItem('email', data.email);
+        if (data.exito && !data.requiereActivacion) {
+          localStorage.setItem('token', data.token);
+          localStorage.setItem('email', data.email);
 
-        console.log('Redirigiendo a crear-plan-entrenamiento...');
-        this.router.navigate(['/planes']);
-      } else if (data.requiereActivacion) {
-        alert('Debes activar tu cuenta antes de iniciar sesión.');
+          console.log('Redirigiendo a crear-plan-entrenamiento...');
+          this.router.navigate(['/planes']);
+        } else if (data.requiereActivacion) {
+          alert('Debes activar tu cuenta antes de iniciar sesión.');
+        }
+      },
+      error: (err) => {
+        console.error('Error al iniciar sesión:', err);
+        alert('Credenciales incorrectas o error de servidor.');
       }
-    },
-    error: (err) => {
-      console.error('Error al iniciar sesión:', err);
-      alert('Credenciales incorrectas o error de servidor.');
-    }
-  });
-}
+    });
+  }
 }
 
