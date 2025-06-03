63be714986ecda4255965164fbe0eefe9ef99261
Facundo Varela <111824912+facuvarela00@users.noreply.github.com>
2025-06-03 20:27:58 -0300

    arreglo app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,6 @@ import { FinalizacionRutinaComponent } from './vistas/finalizacion-rutina/finali
 import { DetallePlanComponent } from './vistas/detalle-plan/detalle-plan.component';
 import { RealizarEjercicioComponent } from './vistas/realizar-ejercicio/realizar-ejercicio.component';
 import { InformacionEjercicioComponent } from './vistas/informacion-ejercicio/informacion-ejercicio.component';
-import { VerificarEmailComponent } from './vistas/verificar-email/verificar-email.component';
 import { PerfilComponent } from './vistas/perfil/perfil.component';
 
 import { authGuard } from './core/guards/auth.guards';
