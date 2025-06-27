e69a1c268f7e5929694ad5ffa65a6b5e8e3bb0d9
LeonardoVilte <vilteleonardo00@gmail.com>
2025-06-26 23:47:08 -0300

    cambios de usuarioServicio

--- src/app/core/servicios/usuarioServicio/usuario.service.ts
+++ src/app/core/servicios/usuarioServicio/usuario.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable} from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { RespuestaApi } from '../../modelos/RespuestaApiDTO';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +11,8 @@ export class UsuarioService {
   private baseUrl = environment.URL_BASE;
 
   constructor(private http: HttpClient) {}
-  obtenerUsuarioPorEmail(email: string | null): Observable<any> {
-    return this.http.get(`${this.baseUrl}/usuario/obtener/${email}`);
+  obtenerUsuarioPorEmail(email: string | null): Observable<RespuestaApi<UsuarioDTO>> {
+    return this.http.get<RespuestaApi<UsuarioDTO>>(`${this.baseUrl}/usuario/obtener/${email}`);
   }
 
 }
